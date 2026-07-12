a677615568576f9caa4ca8a1142b8a46efb50b8b
zLucky090802 <d.espitia15@gmail.com>
2026-07-12 15:08:23 -0500

    feat(app): orchestrate landing page architecture by integrating core standalone modules into root view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeroComponent } from './features/landing/components/hero/hero.component';
+import { OurServicesComponent } from './features/landing/components/our-services/our-services.component';
+import { FormContactComponent } from './features/landing/components/form-contact/form-contact.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { AboutUsComponent } from "./features/landing/components/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    HeroComponent,
+    OurServicesComponent,
+    CasesComponent,
+    FormContactComponent,
+    FooterComponent,
+    AboutUsComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
