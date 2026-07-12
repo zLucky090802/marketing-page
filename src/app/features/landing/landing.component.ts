22acbff7a28ef3540eb75a49b5ea4a333c57ac21
zLucky090802 <d.espitia15@gmail.com>
2026-07-12 14:41:03 -0500

    feat(shared): add responsive navbar component with mobile menu

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { FormContactComponent } from './components/form-contact/form-contact.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ProccessComponent } from './components/proccess/proccess.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    HeroComponent,
+    FormContactComponent,
+    AboutUsComponent,
+    ProccessComponent,
+    OurServicesComponent
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMobileMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
 }
