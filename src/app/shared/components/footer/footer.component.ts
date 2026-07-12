589b014ce77a1819c481a6463cebb4823312e6c0
zLucky090802 <d.espitia15@gmail.com>
2026-07-12 15:06:14 -0500

    feat(landing): create cohesive footer component with dynamic signal-driven year tracking

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  currentYear = signal(new Date().getFullYear());
 }
