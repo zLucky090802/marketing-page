d18163455968d1e32eefd2e537213931a7713c18
zLucky090802 <d.espitia15@gmail.com>
2026-08-13 11:22:01 -0500

    feat(contact): refactor contact form to use Reactive Forms with validation

--- src/app/features/landing/components/form-contact/form-contact.component.ts
+++ src/app/features/landing/components/form-contact/form-contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 interface ContactData {
@@ -22,33 +22,40 @@ export class FormContactComponent {
   isSubmitting = signal(false);
   submitSuccess = signal(false);
   private http = inject(HttpClient);
+  private fb = inject(FormBuilder);
 
-  contactModel = signal<ContactData>({
-    nombre: '',
-    email: '',
-    empresa: '',
-    servicio: 'marketing',
-    mensaje: ''
+
+  contactForm : FormGroup = this.fb.group({
+    name: ['',[Validators.required, Validators.minLength(2)]],
+    email: ['',[Validators.required, Validators.email]],
+    company: ['',[Validators.required]],
+    service: ['Marketing', Validators.required],
+    message: ['',[Validators.required, Validators.minLength(2)]],
   });
 
   onSubmit(event: Event) {
+    if(this.contactForm.invalid){
+      this.contactForm.markAllAsTouched();
+    }
+
     event.preventDefault();
     this.isSubmitting.set(true);
 
-    const data = this.contactModel();
+    const data = this.contactForm.value;;
     console.log('Enviando Lead Calificado:', data);
 
-
+    
     // Simulación de envío reactivo
     setTimeout(() => {
       this.isSubmitting.set(false);
       
-      this.http.post('https://formspree.io/f/meeyelpq', this.contactModel())
+      this.http.post('https://formspree.io/f/meeyelpq', data)
       .subscribe({
         next: (response) =>{
           alert('¡Mensaje enviado con éxito!')
           this.isSubmitting.set(false)
           this.submitSuccess.set(true);
+          this.resetFormState();
         },
         error: (err) =>{
           alert('Hubo un error al enviar el mensaje.')
@@ -56,21 +63,12 @@ export class FormContactComponent {
         }
       })
       // Reseteamos el modelo a su estado inicial
-      this.contactModel.set({
-        nombre: '',
-        email: '',
-        empresa: '',
-        servicio: 'marketing',
-        mensaje: ''
-      });
+      
     }, 1500);
-  }
-
-  updateField(key: keyof ContactData, value: string) {
-    this.contactModel.update(state => ({ ...state, [key]: value }));
   }
 
   resetFormState() {
     this.submitSuccess.set(false);
+    this.contactForm.reset({service:'marketing'})
   }
 }
